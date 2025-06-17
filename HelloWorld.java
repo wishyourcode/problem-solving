dc35355309a8238ac9dc0e84502d3c494878e2bb
wishyourcode <vishalm3101@gmail.com>
2025-06-17 23:54:25 +0530

    1st program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // This is a simple Java program that prints "Hello, World!" to the console.
+    }
+    
+}
