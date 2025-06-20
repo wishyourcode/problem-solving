f3ab7a0e554b1e18e6fa67f0cda346fa5c222937
wishyourcode <vishalm3101@gmail.com>
2025-06-20 20:12:46 +0530

    Add Pattern2.java to print a right-angled triangle pattern of asterisks

---
+++ Pattern2.java
@@ -0,0 +1,13 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        pattern(6);
+    }
+    static void pattern(int n){
+        for(int row =0; row<n; row++){
+            for(int coll=0; coll< row; coll++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
