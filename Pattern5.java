f34062da2ce5e126328cdd7c2d054f056a3d31da
wishyourcode <vishalm3101@gmail.com>
2025-06-21 00:26:04 +0530

    Update Pattern5.java to print a diamond pattern with 7 rows and add space after asterisks

--- Pattern5.java
+++ Pattern5.java
@@ -1,18 +1,18 @@
 public class Pattern5 {
     public static void main(String[] args) {
-        pattern(5);
+        pattern(7);
     }
     static void pattern(int n) {
         for(int row =0; row < 2*n; row ++){
             if(row<=n){
             for(int coll=0; coll< row; coll++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
         else{
             for(int coll=n; coll>row-n; coll--){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
