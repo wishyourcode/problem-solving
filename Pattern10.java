576c3c76284d6bc510ba8571953c4992e33ce8de
wishyourcode <vishalm3101@gmail.com>
2025-06-22 00:29:45 +0530

    Add Pattern10.java to implement a pattern printing program for odd rows with asterisks

--- Pattern10.java
+++ Pattern10.java
@@ -1,3 +1,20 @@
 public class Pattern10 {
-    
+        public static void main(String[] args) {
+        pattern(11);
+    }
+    static void pattern(int n) {
+        for (int row = n; row>0; row--) {
+            if (row % 2 != 0) {
+                for (int space =n; space> row; space--) {
+                    System.out.print(" ");
+                }
+                for (int coll =0; coll< row; coll++) {
+                    System.out.print(" * ");
+                }
+            } else {
+                continue;
+            }
+            System.out.println("");
+        }
+    }
 }
