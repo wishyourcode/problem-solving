8d8f984a7e2857a2d8f2f561502580b0a1991b80
wishyourcode <vishalm3101@gmail.com>
2025-06-22 00:01:13 +0530

    Refactor Pattern9.java for improved formatting and readability; add Pattern10.java as a placeholder for future implementation

--- Pattern9.java
+++ Pattern9.java
@@ -1,19 +1,23 @@
 public class Pattern9 {
     public static void main(String[] args) {
-        pattern( 11);
+        pattern(11);
     }
-    static void pattern(int n){
-        for(int row=n; row>0; row--){
-            for(int space =0; space<row; space++){
-                System.out.print(" ");
-            }
-            for(int coll=n;coll>row;coll--){
-                if(row%2==0){
+//            *
+//          *  *  *
+//        *  *  *  *  *
+//      *  *  *  *  *  *  *
+//    *  *  *  *  *  *  *  *  *
+    static void pattern(int n) {
+        for (int row = n; row > 0; row--) {
+            if (row % 2 == 0) {
+                for (int space = 0; space < row; space++) {
+                    System.out.print(" ");
+                }
+                for (int coll = n; coll > row; coll--) {
                     System.out.print(" * ");
-                } 
-                else{
-                    continue;
-                }  
+                }
+            } else {
+                continue;
             }
             System.out.println("");
         }
