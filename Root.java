e92a09261c3d615230f7ed22817ceedc233654c5
wishyourcode <vishalm3101@gmail.com>
2025-06-26 20:13:12 +0530

    Refactor formatting and spacing in Root.java for improved readability

--- Root.java
+++ Root.java
@@ -3,28 +3,30 @@ public class Root {
     public static void main(String[] args) {
         int n = 40;
         int p = 3;
-        System.out.printf("%.3f",findRoot(n, p));
+        System.out.printf("%.3f", findRoot(n, p));
     }
-    static double findRoot(int n, int p){
+
+    static double findRoot(int n, int p) {
         int start = 0;
         int end = n;
         double root = 0.0;
-        while (start<= end) {
-            int mid = start+ (end-start)/2;
-            if((mid* mid)==n){
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if ((mid * mid) == n) {
                 return mid;
-            }if(mid *mid>n){
-                end = mid-1;
-            }else{
-                start= mid+1;
+            }
+            if (mid * mid > n) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
             double incr = 0.1;
-            for(int i =0; i<p; i++){
-               while(root*root<=n){
-                root = root+incr;
-               } 
-               root = root-incr;
-               incr = incr/10;
+            for (int i = 0; i < p; i++) {
+                while (root * root <= n) {
+                    root = root + incr;
+                }
+                root = root - incr;
+                incr = incr / 10;
             }
         }
         return root;
